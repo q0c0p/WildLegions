63541070b49685b9dc0633f55cabe97305050041
q0c0p <solene.dardennes@edu.esiee.fr>
2015-06-07 22:24:19 +0200

    Player controleur import and set ito the scene

---
+++ Assets/_Scripts/ButterflyBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterflyBehaviourScript : MonoBehaviour {
+	
+	public float mSpeed;
+	private CharacterController mCharacterController;
+	public Animation mAnimation;
+
+	void Start ()
+	{
+		mCharacterController = GetComponent<CharacterController>();
+		mAnimation = GetComponent<Animation>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		mCharacterController.SimpleMove (movement * mSpeed);
+
+		mAnimation.Play("Take 001");
+	}
+}
