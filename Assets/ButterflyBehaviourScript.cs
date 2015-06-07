cfa1b017d75c5b338a41bd5c47bb4f6b09f58570
q0c0p <solene.dardennes@edu.esiee.fr>
2015-06-07 16:48:52 +0200

    First commit

---
+++ Assets/ButterflyBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterflyBehaviourScript : MonoBehaviour {
+
+	public float speed;
+	
+	private CharacterController cc;
+	
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		cc.SimpleMove (movement * speed);
+	}
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject Butterfly;
+	
+	private Vector3 offset;
+	
+	void Start ()
+	{
+		offset = transform.position - Butterfly.transform.position;
+	}
+	
+	void LateUpdate ()
+	{
+		transform.position = Butterfly.transform.position + offset;
+	}
+}
