93bfa0e710288bc423f467451623f16c7fdf9c2c
q0c0p <solene.dardennes@edu.esiee.fr>
2015-06-10 22:28:02 +0200

    deleted player script level1

--- Assets/_Scripts/Player_Scripts/PlayerController.cs
+++ Assets/_Scripts/Player_Scripts/PlayerController.cs
@@ -3,81 +3,48 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	/*
-	public float mSpeed;
-	private CharacterController mCharacterController;
-
-	
-	void Start ()
-	{
-		mCharacterController = GetComponent<CharacterController>();
-
-	}
-	
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		
-		mCharacterController.SimpleMove (movement * mSpeed);
-		
-
-	}
-	*/
-}
-
-/*
-{
-	public float speed = 6f;            // The speed that the player will move at.
-	
-	Vector3 movement;                   // The vector to store the direction of the player's movement.
-	//	Animator anim;                      // Reference to the animator component.
-	CharacterController playerCharacterController;          // Reference to the player's rigidbody.
-	int terrainMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+	float speed = 6f; 
+	float jumpSpeed = 8f; 
+	float turnSpeed = 60f;
+	float gravity = 20f;
 	float camRayLength = 100f;          // The length of the ray from the camera into the scene.
-	
+	int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+
+
 	void Awake ()
 	{
 		// Create a layer mask for the floor layer.
-		terrainMask = LayerMask.GetMask ("Terrain");
+		floorMask = LayerMask.GetMask ("Floor");
 		
 		// Set up references.
-		//		anim = GetComponent <Animator> ();
-		playerCharacterController = GetComponent <CharacterController> ();
+		//anim = GetComponent <Animator> ();
+		controller = GetComponent <CharacterController> ();
+	}
+
+	void Start(){
+	controller = GetComponent<CharacterController>();
 	}
 	
-	
-	void FixedUpdate ()
-	{
-		// Store the input axes.
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
-		
-		// Move the player around the scene.
-		Move (h, v);
-		
-		// Turn the player to face the mouse cursor.
-		Turning ();
-		
-		// Animate the player.
-		Animating (h, v);
+	void FixedUpdate() {
+		if (controller.isGrounded) { 
+			//Quaternion turn = Turning (); 
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			//moveDirection = transform.Rotate(turn); 
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
 	}
-	
-	void Move (float h, float v)
+
+	Quaternion Turning ()
 	{
-		// Set the movement vector based on the axis input.
-		movement.Set (h, 0f, v);
-		
-		// Normalise the movement vector and make it proportional to the speed per second.
-		movement = movement.normalized * speed * Time.deltaTime;
-		
-		// Move the player to it's current position plus the movement.
-		playerCharacterController.Move(transform.position + movement);
-	}
-	
-	void Turning ()
-	{
+		Quaternion newRotation = Quaternion.identity;
 		// Create a ray from the mouse cursor on screen in the direction of the camera.
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		
@@ -85,7 +52,7 @@ public class PlayerController : MonoBehaviour {
 		RaycastHit floorHit;
 		
 		// Perform the raycast and if it hits something on the floor layer...
-		if(Physics.Raycast (camRay, out floorHit, camRayLength, terrainMask))
+		if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
 		{
 			// Create a vector from the player to the point on the floor the raycast from the mouse hit.
 			Vector3 playerToMouse = floorHit.point - transform.position;
@@ -94,19 +61,12 @@ public class PlayerController : MonoBehaviour {
 			playerToMouse.y = 0f;
 			
 			// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			newRotation = Quaternion.LookRotation (playerToMouse);
 			
-			// Set the player's rotation to this new rotation.
-			playerCharacterController.MoveRotation (newRotation);
+
 		}
+		// Set the player's rotation to this new rotation.
+		return (newRotation);
 	}
-	
-	void Animating (float h, float v)
-	{
-		// Create a boolean that is true if either of the input axes is non-zero.
-		bool walking = h != 0f || v != 0f;
-		
-		// Tell the animator whether or not the player is walking.
-		anim.SetBool ("IsWalking", walking);
-	}
-}*/
+	*/
+}
