a8cd8d13dc1d8aeae056124b8a2774a7dcde4009
q0c0p <solene.dardennes@edu.esiee.fr>
2015-06-14 23:29:09 +0200

    create camera prefabs and scripts for 3rd person, follow and mouse aimed

---
+++ Assets/_Scripts/Camera_Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	public GameObject target;
+	Vector3 offset;
+	public float damping = 1;
+
+	void Start() {
+		offset = transform.position - target.transform.position;
+	}
+	
+	void LateUpdate() {
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+		transform.position = position;
+
+		transform.LookAt(target.transform.position);
+	}
+}
