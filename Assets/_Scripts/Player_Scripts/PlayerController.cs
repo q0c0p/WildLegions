bc3b12189c8a66d7aa33674abf9c9b57e3bf458e
q0c0p <solene.dardennes@edu.esiee.fr>
2015-06-15 10:24:58 +0200

    tidy move terrain to Map folder

--- Assets/_Scripts/Player_Scripts/PlayerController.cs
+++ Assets/_Scripts/Player_Scripts/PlayerController.cs
@@ -3,43 +3,46 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 6f;            // The speed that the player will move at.
-		
-	private Vector3 moveDirection = Vector3.zero;                   // The vector to store the direction of the player's movement.
-	private Vector3 rightDirection = Vector3.zero;
-	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-	int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+	public float speed_ = 6f;            // The speed that the player will move at.
+	public float rotateSpeed_ ;	
+	private Vector3 moveDirection_ = Vector3.zero;                   // The vector to store the direction of the player's movement.
+	private Vector3 rightDirection_ = Vector3.zero;
+	Rigidbody playerRigidbody_;          // Reference to the player's rigidbody.
+	int floorMask_;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
 
 	void Awake ()
 		{
 			// Create a layer mask for the floor layer.
-			floorMask = LayerMask.GetMask ("Terrain");
+			floorMask_ = LayerMask.GetMask ("Terrain");
 			
 			// Set up references.
-			playerRigidbody = GetComponent <Rigidbody> ();
+			playerRigidbody_ = GetComponent <Rigidbody> ();
 		}
 		
 		
 	void FixedUpdate ()
 		{
 			// Store the input axes.
-			float h = Input.GetAxisRaw ("Horizontal");
-			float v = Input.GetAxisRaw ("Vertical");
+			float h_ = Input.GetAxisRaw ("Horizontal");
+			float v_ = Input.GetAxisRaw ("Vertical");
 			
 			// Move the player around the scene.
-			Move (h,v);
+			Move (h_,v_);
 		
 		}
 		
 	void Move (float h, float v)
 	{
-		moveDirection.Set (h, 0, v);
+		float horizontal = h * rotateSpeed_;
+		playerRigidbody_.transform.Rotate (0, horizontal, 0);
+
+		moveDirection_.Set (0, 0, v);
 
 		// Normalise the movement vector and make it proportional to the speed per second.
-		moveDirection = moveDirection.normalized * speed * Time.deltaTime;
+		moveDirection_ = moveDirection_.normalized * speed_ * Time.deltaTime;
 
 		// Move the player to it's current position plus the movement.
-		playerRigidbody.MovePosition (transform.position + moveDirection);
+		playerRigidbody_.MovePosition (transform.position + moveDirection_);
 	}
 
 }
