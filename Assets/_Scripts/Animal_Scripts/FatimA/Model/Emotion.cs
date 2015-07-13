c428afdfc4cb17012fe20d50b6b60bf5e70ea766
basketmaker <pierreyves.maes@edu.esiee.fr>
2015-07-13 19:39:53 +0200

    Remove some merging

--- Assets/_Scripts/Animal_Scripts/FatimA/Model/Emotion.cs
+++ Assets/_Scripts/Animal_Scripts/FatimA/Model/Emotion.cs
@@ -2,13 +2,10 @@
 
 namespace Fatima
 {
-	/**
-<<<<<<< HEAD
-	 * This class represents emotions as defined in FatiMA theory. 
-	 * Emotions have an Intensity, meaning how strong it is, and a valence which represent how positive or negative the emotion is. 
-=======
-	 * This interface represents emotions as defined in FatiMA theory. Emotions have an Intensity, meaning how strong it is, and a valence which represent how positive or negative the emotion is. 
->>>>>>> 8f3ece6e5771b390a3709dc7060090e2a78d7954
+	/** 
+	 * This interface represents emotions as defined in FatiMA theory. 
+	 * Emotions have an Intensity, meaning how strong it is, 
+	 * and a valence which represent how positive or negative the emotion is. 
 	 * */
 	public interface Emotion {
 		/**
