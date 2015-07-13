fd70e41e179dc68b56af7d6fdd66ae770c5b1cc1
basketmaker <pierreyves.maes@edu.esiee.fr>
2015-07-13 18:15:02 +0200

    add documentation to fatima model

--- Assets/_Scripts/Animal_Scripts/FatimA/Model/AffectiveState.cs
+++ Assets/_Scripts/Animal_Scripts/FatimA/Model/AffectiveState.cs
@@ -1,7 +1,15 @@
 ﻿
 namespace Fatima
 {
+	/**
+	 * An object to store the current affective state : 
+	 * it contains a set of Emotions
+	 */
 	public interface AffectiveState {
+		/**
+		 * This function add an emotion to the affective state 
+		 * The Emotion is add or update
+		 */
 		void addEmotion(Emotion pemotion);
 	}
 }
--- Assets/_Scripts/Animal_Scripts/FatimA/Model/AppraisalComponent.cs
+++ Assets/_Scripts/Animal_Scripts/FatimA/Model/AppraisalComponent.cs
@@ -1,7 +1,14 @@
 ﻿
 namespace Fatima
 {
+	/**
+	 * the Appraisal Component is an object of the Appraisal Derivation process of the FAtiMA model
+	 * this inteface is implemented into several objects which affect the Appraisal of an Event
+	 */
 	public interface AppraisalComponent : Fatima.Component {
+		/**
+		 * 
+		 */
 		void startAppraisal(FatimaEvent evenement, AppraisalFrame aF);
 		AppraisalFrame continuousAppraisal();
 	}
