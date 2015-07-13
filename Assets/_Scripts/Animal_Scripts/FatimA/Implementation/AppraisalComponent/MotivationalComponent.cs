7cb3b7346eec6158a0867ad00c5c755a5a0ce188
q0c0p <solene.dardennes@edu.esiee.fr>
2015-07-13 19:27:53 +0200

    new component MotivationalComponent

---
+++ Assets/_Scripts/Animal_Scripts/FatimA/Implementation/AppraisalComponent/MotivationalComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fatima;
+
+/**
+ * This component models the basic agent's drives (=needs) such as energy, health... 
+ * When updated by an event, it evaluates its desirability (appraisal varible) and sends it to the current appraisal frame. 
+ * */
+public class MotivationalComponent : AppraisalComponent {
+
+	private AppraisalVariable desirability_;
+
+	void update();
+	void update(FatimaEvent pevent);
+	void startAppraisal(FatimaEvent evenement, AppraisalFrame aF);
+	AppraisalFrame continuousAppraisal();
+}
--- Assets/_Scripts/Animal_Scripts/FatimA/Model/Emotion.cs
+++ Assets/_Scripts/Animal_Scripts/FatimA/Model/Emotion.cs
@@ -3,7 +3,8 @@
 namespace Fatima
 {
 	/**
-	 * This class represents emotions as defined in FatiMA theory. Emotions have an Intensity, meaning how strong it is, and a valence which represent how positive or negative the emotion is. 
+	 * This class represents emotions as defined in FatiMA theory. 
+	 * Emotions have an Intensity, meaning how strong it is, and a valence which represent how positive or negative the emotion is. 
 	 * */
 	public interface Emotion {
 		/**
