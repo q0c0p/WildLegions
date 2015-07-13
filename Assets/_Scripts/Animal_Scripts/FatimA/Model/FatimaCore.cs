74fc8f9f11e9f10f22e5c905f1604b4db83fdff3
basketmaker <pierreyves.maes@edu.esiee.fr>
2015-07-13 19:11:57 +0200

    End of FAtiMA model documentation

--- Assets/_Scripts/Animal_Scripts/FatimA/Model/FatimaCore.cs
+++ Assets/_Scripts/Animal_Scripts/FatimA/Model/FatimaCore.cs
@@ -2,10 +2,25 @@
 namespace Fatima
 {
 	/**
-	 * 
+	 * This interface is between the the AnimalController and the FAtiMA implementation.
+	 * The pseudo code of FAtiMA is provided in the abstract class FatimaCoreAbstract .
 	 * */
 	public interface FatimaCore {
-		void sendevent(FatimaEvent pevent);
+		/**
+		 * The update method is call whenever the developper want .
+		 * This update will update the components in FAtiMA.
+		 */
+		void update();
+		/**
+		 * This method is used to update the FAtiMA when he received an event. 
+		 * This event can be several ones like received food, trigger an attack.
+		 * We send this event to FAtiMA and FAtiMA will the update his state.
+		 * */
+		void sendEvent(FatimaEvent pevent);
+		/**
+		 * This Method get the Action which has been decided by the FAtiMA component
+		 * It is usually called by agent controller to execuate the action
+		 * */
 		Action getAction();
 	}
 }
