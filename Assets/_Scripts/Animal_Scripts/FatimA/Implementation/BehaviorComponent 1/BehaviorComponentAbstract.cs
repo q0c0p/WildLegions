7966cca76c0c5b88a40b433589d43014726b71fb
basketmaker <pierreyves.maes@edu.esiee.fr>
2015-07-24 09:58:21 +0200

    end for this morning 24/07/2015

---
+++ Assets/_Scripts/Animal_Scripts/FatimA/Implementation/BehaviorComponent 1/BehaviorComponentAbstract.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BehaviorComponentAbstract : Fatima.BehaviorComponent {
+
+
+
+	public BehaviorComponentAbstract()
+	{
+	}
+	/**
+	 * not yet implemented 
+	 * this should update the hungriness
+	 * */
+	public void update()
+	{
+	}
+	/**
+	 * not yet implemented 
+	 * this should update the hungriness
+	 * */
+	public void update(FatimaEvent pevent)
+	{
+	}
+
+
+	public abstract void actionSelection();
+	
+}
---
+++ Assets/_Scripts/Animal_Scripts/FatimA/Implementation/BehaviorComponent 1/DeliberativeComponent.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class DeliberativeComponent : BehaviorComponentAbstract
+{
+	public DeliberativeComponent ()
+	{
+	}
+	public override void actionSelection ()
+	{
+	}
+}
+
+
